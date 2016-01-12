1696ac79a69ed4f90477b9718393a0407d6d335c
fcopardo <francisco.pardo.baeza@gmail.com>
2016-01-12 04:17:39 -0300

    added POST example

--- src/main/java/com/grizzly/restServices/Services/ExampleService.java
+++ src/main/java/com/grizzly/restServices/Services/ExampleService.java
@@ -11,11 +11,10 @@ public class ExampleService {
 
 
     public static GenericRestCall<Void, Void, Void> postCall(afterTaskCompletion taskCompletion){
-        GenericRestCall<Void, Void, Void> restCall = new GenericRestCall<>(Void.class, Void.class, Void.class);
-        restCall.setMethodToCall(HttpMethod.POST);
-        restCall.setUrl("http://localhost:8082/hello/post");
-        restCall.setTaskCompletion(taskCompletion);
-        return restCall;
+        return new GenericRestCall<>(Void.class, Void.class, Void.class)
+        .setMethodToCall(HttpMethod.POST)
+        .setUrl("http://localhost:8082/hello/post")
+        .setTaskCompletion(taskCompletion);
     }
 
 }
